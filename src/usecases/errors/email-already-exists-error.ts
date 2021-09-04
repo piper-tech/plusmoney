b45096e472f6c78e533c851d2cbf36bd50949a8d
Gustavo <alves.gustavocunha@gmail.com>
2021-09-04 12:43:22 -0300

    feat: add email-already--exists-error

---
+++ src/usecases/errors/email-already-exists-error.ts
@@ -0,0 +1,8 @@
+import { UseCaseError } from "./usecase-error";
+
+export class EmailAlreadyExistsError extends Error implements UseCaseError {
+    constructor(email: string){
+        super(`this email ${email} already exists`);
+        this.name = 'EmailAlreadyExistsError';
+    }
+}
