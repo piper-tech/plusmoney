7d5549cc770829707a11d2586bf75cbf3b56855c
Gustavo <alves.gustavocunha@gmail.com>
2021-09-06 11:57:48 -0300

    feat: add invalid-name-error

---
+++ src/entities/errors/invalid-name-error.ts
@@ -0,0 +1,8 @@
+import { EntityError } from "./entity-error";
+
+export class InvalidNameError extends Error implements EntityError {
+    constructor() {
+        super(`this name is invalid`);
+        this.name = 'InvalidNameError';
+    }
+}
