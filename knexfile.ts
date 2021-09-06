e3478f9bec0e06c06b4f49260662e2919351427f
Gustavo <alves.gustavocunha@gmail.com>
2021-09-06 15:46:38 -0300

    build: change db_host to use a .env variable

--- knexfile.ts
+++ knexfile.ts
@@ -9,7 +9,7 @@ const knexConfig: KnexConfig = {
   development: {
     client: "mysql2",
     connection: {
-      host : 'database',
+      host : process.env.DB_HOST,
       database: process.env.DB_NAME,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD
