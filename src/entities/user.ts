562128a7602d72fdc9e74a8db30b534e1443bb86
Gustavo <alves.gustavocunha@gmail.com>
2021-09-06 11:59:58 -0300

    refactor: add factory method to create user with validations

--- src/entities/user.ts
+++ src/entities/user.ts
@@ -1,12 +1,40 @@
-export class User {
-    public name: string;
-    public email: string;
-    public password: string;
+import { Name } from "./value-objects/name";
+import { Email } from "./value-objects/email";
+import { Password } from "./value-objects/password";
+import { left, right, Either } from "../shared/either";
+import { InvalidNameError } from "./errors/invalid-name-error";
+import { InvalidEmailError } from "./errors/invalid-email-error";
+import { InvalidPasswordError } from "./errors/invalid-password-error";
+import { CreateUserDTO } from "../usecases/create-user/create-user-dto";
 
-    constructor(name: string, email: string, password: string) {
+export class User {
+    readonly name: Name;
+    readonly email: Email;
+    readonly password: Password;
+
+    private constructor(name: Name, email: Email, password: Password) {
         this.name = name;
         this.email = email;
         this.password = password;
+    }
+
+    static create(data: CreateUserDTO): Either<InvalidNameError | InvalidEmailError | InvalidPasswordError, User> {
+        const nameOrError = Name.create(data.name);
+        const emailOrError = Email.create(data.email);
+        const passwordOrError = Password.create(data.password);
+        if(nameOrError.isLeft()){
+            return left(nameOrError.value);
+        }
+        if(emailOrError.isLeft()){
+            return left(emailOrError.value);
+        }
+        if(passwordOrError.isLeft()){
+            return left(passwordOrError.value);
+        }
+        const name: Name = nameOrError.value;
+        const email: Email = emailOrError.value;
+        const password: Password = passwordOrError.value;
+        return right(new User(name, email, password));
     }
 
 }
