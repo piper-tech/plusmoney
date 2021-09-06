79a6db210f2660ea12183b21329da20f38459c22
Gustavo <alves.gustavocunha@gmail.com>
2021-09-06 11:55:46 -0300

    feat: add password object value

---
+++ src/entities/value-objects/password.ts
@@ -0,0 +1,28 @@
+import { right, left, Either } from "../../shared/either";
+import { InvalidPasswordError } from "../errors/invalid-password-error";
+
+export class Password {
+    readonly password: string;
+
+    private constructor(password: string){
+        this.password = password;
+    }
+
+    static create(password: string): Either<InvalidPasswordError, Password> {
+        if(!Password.validate(password)){
+            return left(new InvalidPasswordError());
+        }
+        return right(new Password(password));
+    }
+
+    static validate(password: string): boolean {
+        if(!password){
+            return false
+        }
+        return true;
+    }
+
+    get value(): string {
+        return this.password;
+    }
+}
