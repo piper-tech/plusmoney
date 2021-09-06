738601948c41ce78b2df34f27e338eea9c72af9a
Gustavo <alves.gustavocunha@gmail.com>
2021-09-06 12:00:36 -0300

    refactor: add errors to create-user-response

--- src/usecases/create-user/create-user-response.ts
+++ src/usecases/create-user/create-user-response.ts
@@ -1,5 +1,11 @@
+import { InvalidEmailError } from '../../entities/errors/invalid-email-error';
+import { InvalidNameError } from '../../entities/errors/invalid-name-error';
+import { InvalidPasswordError } from '../../entities/errors/invalid-password-error';
 import { Either } from '../../shared/either';
 import { EmailAlreadyExistsError } from '../errors/email-already-exists-error';
 import { CreateUserDTO } from './create-user-dto';
 
-export type CreateUserResponse = Either<EmailAlreadyExistsError, CreateUserDTO>
+export type CreateUserResponse = Either<EmailAlreadyExistsError | 
+InvalidEmailError | 
+InvalidNameError | 
+InvalidPasswordError, CreateUserDTO>
