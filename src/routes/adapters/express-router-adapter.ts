80773de76663a8f7eadcaea61d5ebc0729f4df20
Gustavo <alves.gustavocunha@gmail.com>
2021-09-07 12:49:45 -0300

    refactor: move routerAdapter to own file

---
+++ src/routes/adapters/express-router-adapter.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from 'express';
+import { Controller } from '@/controllers';
+
+export function routerAdapter(controller: Controller) {
+  return async (req: Request, res: Response) => {
+    const request = {
+      ...(req.body || {}),
+      ...(req.params || {})
+    };
+    const httpResponse = await controller.handler(request);
+    if (httpResponse.statusCode >= 200 && httpResponse.statusCode <= 299) {
+      return res.status(httpResponse.statusCode).json(httpResponse.body);
+    }
+    return res.status(httpResponse.statusCode).json({ error: httpResponse.body.message });
+  };
+}
--- src/routes/user-routes.ts
+++ src/routes/user-routes.ts
@@ -1,20 +1,6 @@
-import { Router, Request, Response } from 'express';
+import { Router } from 'express';
 import { CreateUserController } from '@/controllers/implementations';
-import { Controller } from '@/controllers';
-
-function routerAdapter(controller: Controller) {
-  return async (req: Request, res: Response) => {
-    const request = {
-      ...(req.body || {}),
-      ...(req.params || {})
-    };
-    const httpResponse = await controller.handler(request);
-    if (httpResponse.statusCode >= 200 && httpResponse.statusCode <= 299) {
-      return res.status(httpResponse.statusCode).json(httpResponse.body);
-    }
-    return res.status(httpResponse.statusCode).json({ error: httpResponse.body.message });
-  };
-}
+import { routerAdapter } from '@/routes/adapters';
 
 export default (router: Router) => {
   router.post('/users', routerAdapter(new CreateUserController()));
