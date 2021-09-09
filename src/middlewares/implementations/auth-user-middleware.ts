fea447911f0c7f76139604819d4f0b28654ddb39
Gustavo <alves.gustavocunha@gmail.com>
2021-09-08 23:21:06 -0300

    refactor: use unauthorized error in auth-user-middleware

--- src/middlewares/implementations/auth-user-middleware.ts
+++ src/middlewares/implementations/auth-user-middleware.ts
@@ -2,6 +2,7 @@ import { HttpResponse } from '@/controllers';
 import { Middleware } from '../middleware';
 import { HttpHelper } from '@/controllers/helpers';
 import { AuthenticationJwt } from '@/providers/implementations';
+import { Unauthorized } from '@/controllers/errors';
 
 interface AuthMiddlewareData {
   accessToken: string;
@@ -13,12 +14,12 @@ export class AuthUserMiddleware implements Middleware {
   async handler(request: AuthMiddlewareData): Promise<HttpResponse> {
     const { accessToken } = request;
     if (!accessToken) {
-      return HttpHelper.unauthorized(new Error('unauthorized'));
+      return HttpHelper.unauthorized(new Unauthorized('unauthorized'));
     }
     const authenticated = await this.authenticationProvider.verify(accessToken);
 
     if (!authenticated) {
-      return HttpHelper.unauthorized(new Error('unauthorized'));
+      return HttpHelper.unauthorized(new Unauthorized('unauthorized'));
     }
 
     return HttpHelper.ok({ message: 'ok' });
