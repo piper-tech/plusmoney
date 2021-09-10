f98dc470aac2a838b240e83a5611c5f8532d97b6
Gustavo <alves.gustavocunha@gmail.com>
2021-09-09 23:08:02 -0300

    fix: add description attribute to entries

--- src/entities/data-transfer-objects/entry-data.ts
+++ src/entities/data-transfer-objects/entry-data.ts
@@ -1,4 +1,5 @@
 export interface EntryData {
+  description: string;
   value: number;
   entryDate: Date;
 }
--- src/repositories/implementations/mysql/migrations/20210910001433_create_table_entry.ts
+++ src/repositories/implementations/mysql/migrations/20210910001433_create_table_entry.ts
@@ -3,6 +3,7 @@ import { Knex } from 'knex';
 export async function up(knex: Knex): Promise<void> {
   return knex.schema.createTable('entries', (table) => {
     table.increments('id');
+    table.text('description').notNullable();
     table.double('value').notNullable();
     table.date('entry_date').notNullable();
   });
