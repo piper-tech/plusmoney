60a80e687ab7f3af9c4d0ea2c87961df01c9c737
Gustavo <alves.gustavocunha@gmail.com>
2021-09-09 23:02:00 -0300

    feat: implements entry-mysql-repository

---
+++ src/repositories/implementations/mysql/entry-mysql-repository.ts
@@ -0,0 +1,15 @@
+import { EntryData } from '@/entities/data-transfer-objects';
+import { EntryRepository } from '@/repositories/entry-repository';
+import knex from './knex';
+
+export class EntryMsqlRepository implements EntryRepository {
+  async save(data: EntryData): Promise<boolean> {
+    try {
+      await knex('entries').insert(data);
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  }
+}
