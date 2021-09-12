8ef2f2c57b4955ccd84336decde1f05d1d58c944
Gustavo <alves.gustavocunha@gmail.com>
2021-09-12 13:10:31 -0300

    fix: change entry-mysql-repository class name

--- src/repositories/implementations/mysql/entry-mysql-repository.ts
+++ src/repositories/implementations/mysql/entry-mysql-repository.ts
@@ -2,7 +2,7 @@ import { EntryData } from '@/entities/data-transfer-objects';
 import { EntryRepository } from '@/repositories/entry-repository';
 import knex from './knex';
 
-export class EntryMsqlRepository implements EntryRepository {
+export class EntryMysqlRepository implements EntryRepository {
   async save(data: EntryData): Promise<boolean> {
     try {
       await knex('entries').insert(data);
