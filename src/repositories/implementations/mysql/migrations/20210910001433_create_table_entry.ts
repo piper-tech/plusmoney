d6573f36430e0d06f6a8deb76a1228e10052311e
Gustavo <alves.gustavocunha@gmail.com>
2021-09-12 23:18:01 -0300

    feat: references between entries and users table

--- src/repositories/implementations/mysql/migrations/20210910001433_create_table_entry.ts
+++ src/repositories/implementations/mysql/migrations/20210910001433_create_table_entry.ts
@@ -2,10 +2,11 @@ import { Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
   return knex.schema.createTable('entries', (table) => {
-    table.increments('id');
-    table.text('description').notNullable();
+    table.increments('id').primary();
+    table.integer('userId').unsigned().notNullable().references('id').inTable('users');
+    table.string('description').notNullable();
     table.double('value').notNullable();
-    table.text('date').notNullable();
+    table.string('date').notNullable();
   });
 }
 
