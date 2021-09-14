dbe224427c3c71afce5fc66cbde3822738b4eccd
Gustavo <alves.gustavocunha@gmail.com>
2021-09-14 20:25:26 -0300

    feat: add get-user router

--- src/routes/user-routes.ts
+++ src/routes/user-routes.ts
@@ -1,7 +1,8 @@
 import { Router } from 'express';
-import { CreateUserController } from '@/controllers/implementations';
+import { CreateUserController, GetUserController } from '@/controllers/implementations';
 import { routerAdapter } from '@/routes/adapters';
 
 export default (router: Router) => {
   router.post('/users', routerAdapter(new CreateUserController()));
+  router.get('/users', routerAdapter(new GetUserController()));
 };
