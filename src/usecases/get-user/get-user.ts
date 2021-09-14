f942033bf6c3b0f6aee5b327d7c92010ae3b750b
Gustavo <alves.gustavocunha@gmail.com>
2021-09-14 19:58:13 -0300

    feat: implements get-user usecase

---
+++ src/usecases/get-user/get-user.ts
@@ -0,0 +1,22 @@
+import { UserRepository } from '@/repositories';
+import { left, right } from '@/shared';
+import { UserNotFoundError } from '../errors/user-not-found-error';
+import { GetUserData } from './get-user-data';
+import { GetUserResponse } from './get-user-response';
+
+export class GetUserUseCase {
+  private userRepository: UserRepository;
+
+  constructor(userRepository: UserRepository) {
+    this.userRepository = userRepository;
+  }
+
+  async execute(data: GetUserData): Promise<GetUserResponse> {
+    const user = await this.userRepository.findByEmail(data.email);
+    if (!user) {
+      return left(new UserNotFoundError());
+    }
+    delete user.password;
+    return right(user);
+  }
+}
