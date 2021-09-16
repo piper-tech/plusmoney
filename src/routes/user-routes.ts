2a790e0be2749c48eede2149fdb1f0ef7601c204
Gustavo <alves.gustavocunha@gmail.com>
2021-09-15 21:19:28 -0300

    refactor: delete create-user-controller and routes

--- src/routes/user-routes.ts
+++ src/routes/user-routes.ts
@@ -1,8 +1,7 @@
 import { Router } from 'express';
-import { CreateUserController, GetUserController } from '@/controllers/implementations';
+import { GetUserController } from '@/controllers/implementations';
 import { routerAdapter } from '@/routes/adapters';
 
 export default (router: Router) => {
-  router.post('/users', routerAdapter(new CreateUserController()));
   router.get('/users', routerAdapter(new GetUserController()));
 };
