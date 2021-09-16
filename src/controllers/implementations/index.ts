6d1ee336363ebf9e4fb86e79b38cecdbcd9ce6d7
Gustavo <alves.gustavocunha@gmail.com>
2021-09-15 21:20:54 -0300

    feat: implements logup-controller

--- src/controllers/implementations/index.ts
+++ src/controllers/implementations/index.ts
@@ -1,4 +1,4 @@
-export * from './create-user-controller';
 export * from './login-controller';
 export * from './create-entry-controller';
 export * from './get-user-controller';
+export * from './logup-controller';
---
+++ src/controllers/implementations/logup-controller.ts
@@ -0,0 +1,32 @@
+import { Controller, HttpResponse } from '@/controllers';
+import { AuthenticateUserUseCase, AuthUserData } from '@/usecases/auth-user';
+import { UserMysqlRepository } from '@/repositories/implementations';
+import { AuthenticationJwt } from '@/providers/implementations';
+import { HttpHelper } from '@/controllers/helpers';
+import { CreateUserUseCase } from '@/usecases/create-user';
+import { UserData } from '@/entities/data-transfer-objects';
+
+export class LogupController implements Controller {
+  private authenticateUser = new AuthenticateUserUseCase(new AuthenticationJwt(), new UserMysqlRepository());
+  private createUser = new CreateUserUseCase(new UserMysqlRepository());
+
+  async handler(request: UserData): Promise<HttpResponse> {
+    try {
+      const userOrError = await this.createUser.execute(request);
+      if (userOrError.isLeft()) {
+        return HttpHelper.badRequest(userOrError.value);
+      }
+      const authUserData: AuthUserData = {
+        email: userOrError.value.email,
+        password: userOrError.value.password as string
+      };
+      const authResponseOrError = await this.authenticateUser.execute(authUserData);
+      if (authResponseOrError.isLeft()) {
+        return HttpHelper.created({ message: 'user created successfully, but unable to authenticate' });
+      }
+      return HttpHelper.ok(authResponseOrError.value);
+    } catch {
+      return HttpHelper.serverError();
+    }
+  }
+}
