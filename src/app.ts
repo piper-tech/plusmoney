49041c89fa2070ae86cef1d9374ec8c556702be9
Gustavo <alves.gustavocunha@gmail.com>
2021-09-17 22:50:34 -0300

    build: config swagger

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import { router } from './routes';
 import cors from 'cors';
+import { serve, setup } from 'swagger-ui-express';
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use('/docs', serve, setup());
 app.use(router);
 
 export { app };
