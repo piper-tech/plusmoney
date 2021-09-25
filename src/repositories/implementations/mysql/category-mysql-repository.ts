8c3e88d97099347e7e96a722dcb278c945e4adfc
Gustavo <alves.gustavocunha@gmail.com>
2021-09-25 15:05:54 -0300

    refactor: implements either error handling in a mysql repositories

--- src/repositories/implementations/mysql/category-mysql-repository.ts
+++ src/repositories/implementations/mysql/category-mysql-repository.ts
@@ -1,15 +1,17 @@
 import { CategoryData } from '@/entities/data-transfer-objects';
-import { CategoryRepository } from '@/repositories';
+import { CategoryRepository, SaveResponse } from '@/repositories/category-repository';
+import { SaveError } from '@/repositories/errors';
+import { left, right } from '@/shared';
 import knex from './knex';
 
 export class CategoryMysqlRepository implements CategoryRepository {
-  async save(data: CategoryData): Promise<boolean> {
+  async save(data: CategoryData): Promise<SaveResponse> {
     try {
       await knex('categories').insert(data);
-      return true;
+      return right(data);
     } catch (error) {
       console.log(error);
-      return false;
+      return left(new SaveError((error as Error).message));
     }
   }
 }
--- src/repositories/implementations/mysql/user-mysql-repository.ts
+++ src/repositories/implementations/mysql/user-mysql-repository.ts
@@ -1,20 +1,26 @@
 import { UserData } from '@/entities/data-transfer-objects';
-import { UserRepository } from '@/repositories';
+import { UserRepository, SaveResponse, FindByEmailResponse } from '@/repositories';
+import { SaveError, FindByEmailError } from '@/repositories/errors';
+import { left, right } from '@/shared';
 import knex from './knex';
 
 export class UserMysqlRepository implements UserRepository {
-  async save(data: UserData): Promise<boolean> {
+  async save(data: UserData): Promise<SaveResponse> {
     try {
       await knex('users').insert(data);
-      return true;
+      return right(data);
     } catch (error) {
-      console.log(error);
-      return false;
+      return left(new SaveError((error as Error).message));
     }
   }
 
-  async findByEmail(email: string): Promise<UserData | undefined> {
-    const user = await knex('users').select().where({ email: email });
-    return user[0];
+  async findByEmail(email: string): Promise<FindByEmailResponse> {
+    try {
+      const user = await knex('users').select().where({ email: email });
+      return right(user[0]);
+    } catch (error) {
+      console.log(error);
+      return left(new FindByEmailError((error as Error).message));
+    }
   }
 }
