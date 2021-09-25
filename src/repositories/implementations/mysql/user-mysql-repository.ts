ad2cd1dd11401d5ba2753628ddb7ba42b213217e
Gustavo <alves.gustavocunha@gmail.com>
2021-09-25 16:23:43 -0300

    refactor: return user id in save method

--- src/repositories/implementations/mysql/user-mysql-repository.ts
+++ src/repositories/implementations/mysql/user-mysql-repository.ts
@@ -7,23 +7,20 @@ import knex from './knex';
 export class UserMysqlRepository implements UserRepository {
   async save(data: UserData): Promise<SaveResponse> {
     try {
-      await knex('users').insert(data);
+      const ids = await knex('users').insert(data);
+      data.id = ids[0];
       return right(data);
     } catch (error) {
+      console.log(error);
       return left(new SaveError((error as Error).message));
     }
   }
 
   async findByEmail(email: string): Promise<FindByEmailResponse> {
-    try {
-      const user = await knex('users').select().where({ email: email });
+    const user = await knex('users').select().where({ email: email });
     if (!user[0]) {
       return left(new FindByEmailError('user not found'));
     }
-      return right(user[0]);
-    } catch (error) {
-      console.log(error);
-      return left(new FindByEmailError((error as Error).message));
-    }
+    return right(user[0]);
   }
 }
