04d990e3e777d777619b6b5301a23cd53b3d503a
Gustavo <alves.gustavocunha@gmail.com>
2021-09-25 15:04:39 -0300

    refactor: add either error handling to repositories

--- src/repositories/category-repository.ts
+++ src/repositories/category-repository.ts
@@ -1,5 +1,9 @@
 import { CategoryData } from '@/entities/data-transfer-objects/category-data';
+import { Either } from '@/shared';
+import { SaveError } from './errors';
+
+export type SaveResponse = Either<SaveError, CategoryData>;
 
 export interface CategoryRepository {
-  save(data: CategoryData): Promise<boolean>
+  save(data: CategoryData): Promise<SaveResponse>
 }
--- src/repositories/entry-repository.ts
+++ src/repositories/entry-repository.ts
@@ -1,6 +1,10 @@
 import { EntryData } from '@/entities/data-transfer-objects';
+import { Either } from '@/shared';
+import { SaveError } from './errors';
+
+export type SaveResponse = Either<SaveError, EntryData>;
 
 export interface EntryRepository {
-  save(data: EntryData): Promise<boolean>
+  save(data: EntryData): Promise<SaveResponse>
   // getAll(): Promise<EntryData[]>
 }
