1de72992062b590769179cc3d373145f31b83007
Gustavo <alves.gustavocunha@gmail.com>
2021-09-25 17:45:41 -0300

    fix: invalid-category-error message

--- src/entities/errors/invalid-category-error.ts
+++ src/entities/errors/invalid-category-error.ts
@@ -2,7 +2,7 @@ import { EntityError } from '.';
 
 export class InvalidCategoryError extends Error implements EntityError {
   constructor() {
-    super('invalid categorty');
+    super('invalid category');
     this.name = 'InvalidCategoryError';
   }
 }
