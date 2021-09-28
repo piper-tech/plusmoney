e543d22bc1d936f892fe08cb2d957915d05b01d5
Gustavo <alves.gustavocunha@gmail.com>
2021-09-27 22:53:54 -0300

    feat: add find method in entry-repository

--- src/repositories/entry-repository.ts
+++ src/repositories/entry-repository.ts
@@ -1,10 +1,12 @@
 import { EntryData } from '@/entities/data-transfer-objects';
 import { Either } from '@/shared';
+import { GetEntryData } from '@/usecases/get-entry';
 import { SaveError } from './errors';
 
 export type SaveResponse = Either<SaveError, EntryData>;
+export type GenericResponse = Either<Error, EntryData[]>;
 
 export interface EntryRepository {
   save(data: EntryData): Promise<SaveResponse>
-  // getAll(): Promise<EntryData[]>
+  find(data: GetEntryData): Promise<GenericResponse>;
 }
