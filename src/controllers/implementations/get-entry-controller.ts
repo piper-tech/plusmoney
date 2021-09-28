ed96f388de6c5ceeda1df12b1ad441d349ebda94
Gustavo <alves.gustavocunha@gmail.com>
2021-09-28 19:37:05 -0300

    feat: get-entry-controller

---
+++ src/controllers/implementations/get-entry-controller.ts
@@ -0,0 +1,17 @@
+import { Controller, HttpResponse } from '@/controllers';
+import { EntryMysqlRepository } from '@/repositories/implementations';
+import { GetEntryData, GetEntryUseCase } from '@/usecases/get-entry';
+import { HttpHelper } from '../helpers';
+
+export class GetEntryController implements Controller {
+  private getEntry = new GetEntryUseCase(new EntryMysqlRepository());
+
+  async handler(request: GetEntryData): Promise<HttpResponse> {
+    const getEntryOrError = await this.getEntry.execute(request);
+    if (getEntryOrError.isLeft()) {
+      return HttpHelper.badRequest(getEntryOrError.value);
+    }
+    const entries = getEntryOrError.value;
+    return HttpHelper.created(entries);
+  }
+}
