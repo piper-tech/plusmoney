74c8983caac9f982f8f4ded97a0c49b07bac4096
Gustavo <alves.gustavocunha@gmail.com>
2021-09-27 22:57:58 -0300

    feat: add get-entry usecase

---
+++ src/usecases/get-entry/get-entry.ts
@@ -0,0 +1,21 @@
+import { EntryRepository } from '@/repositories';
+import { left, right } from '@/shared';
+import { EntryNotFoundError } from '../errors';
+import { GetEntryData } from './get-entry-data';
+import { GetEntryResponse } from './get-entry-response';
+
+export class GetEntryUseCase {
+  private entryRepository;
+
+  constructor(entryRepository: EntryRepository) {
+    this.entryRepository = entryRepository;
+  }
+
+  async execute(data: GetEntryData): Promise<GetEntryResponse> {
+    const entriesOrError = await this.entryRepository.find(data);
+    if (entriesOrError.isLeft()) {
+      return left(new EntryNotFoundError());
+    }
+    return right(entriesOrError.value);
+  }
+}
