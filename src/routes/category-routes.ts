e873717182696b49c83a38745edfb11392c2c4e4
Gustavo <alves.gustavocunha@gmail.com>
2021-10-05 22:23:34 -0300

    feat: add categories get router

--- src/routes/category-routes.ts
+++ src/routes/category-routes.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
-import { CreateCategoryController } from '@/controllers/implementations';
+import { CreateCategoryController, GetCategoryController } from '@/controllers/implementations';
 import { routerAdapter, middlewareAdapter } from '@/routes/adapters';
 import { AuthUserMiddleware } from '@/middlewares/implementations';
 
 export default (router: Router) => {
   router.post('/categories', middlewareAdapter(new AuthUserMiddleware()), routerAdapter(new CreateCategoryController()));
+  router.get('/categories', middlewareAdapter(new AuthUserMiddleware()), routerAdapter(new GetCategoryController()));
 };
