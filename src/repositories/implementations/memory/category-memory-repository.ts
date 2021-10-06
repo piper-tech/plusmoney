46e9fde25b0f2d7e5b43ab7a641311d5dc084d2f
Gustavo <alves.gustavocunha@gmail.com>
2021-10-05 22:20:08 -0300

    feat: implements find method in repositories implementations

--- src/repositories/implementations/memory/category-memory-repository.ts
+++ src/repositories/implementations/memory/category-memory-repository.ts
@@ -1,6 +1,8 @@
 import { CategoryData } from '@/entities/data-transfer-objects';
+import { FindResponse } from '@/repositories';
 import { CategoryRepository, SaveResponse } from '@/repositories/category-repository';
-import { right } from '@/shared';
+import { left, right } from '@/shared';
+import { GetCategoryData } from '@/usecases/get-category';
 
 export class CategoryMemoryRepository implements CategoryRepository {
   private categoryData: CategoryData[] = [];
@@ -8,5 +10,16 @@ export class CategoryMemoryRepository implements CategoryRepository {
   async save(data: CategoryData): Promise<SaveResponse> {
     this.categoryData.push(data);
     return right(data);
+  }
+
+  async find(data: GetCategoryData): Promise<FindResponse> {
+    const categories = this.categoryData.filter(category =>
+      category.description === data.description ||
+      category.userId === data.userId
+    );
+    if (categories.length === 0) {
+      return left(new Error());
+    }
+    return right(categories);
   }
 }
--- src/repositories/implementations/mysql/category-mysql-repository.ts
+++ src/repositories/implementations/mysql/category-mysql-repository.ts
@@ -1,7 +1,9 @@
 import { CategoryData } from '@/entities/data-transfer-objects';
+import { FindResponse } from '@/repositories';
 import { CategoryRepository, SaveResponse } from '@/repositories/category-repository';
 import { SaveError } from '@/repositories/errors';
 import { left, right } from '@/shared';
+import { GetCategoryData } from '@/usecases/get-category';
 import knex from './knex';
 
 export class CategoryMysqlRepository implements CategoryRepository {
@@ -12,6 +14,20 @@ export class CategoryMysqlRepository implements CategoryRepository {
     } catch (error) {
       console.log(error);
       return left(new SaveError((error as Error).message));
+    }
+  }
+
+  async find(data: GetCategoryData): Promise<FindResponse> {
+    try {
+      console.log(data);
+      const categories = await knex('categories').select<CategoryData[]>().where(data);
+      if (categories.length === 0) {
+        return left(new Error());
+      }
+      return right(categories);
+    } catch (error) {
+      console.log(error);
+      return left(new Error((error as Error).message));
     }
   }
 }
