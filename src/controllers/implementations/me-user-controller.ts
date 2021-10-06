e5a19bcc54c75e9949ca453278fb563907b6eba7
Gustavo <alves.gustavocunha@gmail.com>
2021-10-05 22:22:55 -0300

    refactor: use accessToken attribute in MeUserController

--- src/controllers/implementations/me-user-controller.ts
+++ src/controllers/implementations/me-user-controller.ts
@@ -1,6 +1,5 @@
 import { Controller, HttpResponse } from '@/controllers';
 import { HttpHelper } from '@/controllers/helpers';
-import { AuthData } from '@/providers';
 import { AuthenticationJwt } from '@/providers/implementations';
 import { UserMysqlRepository } from '@/repositories/implementations';
 import { GetUserUseCase } from '@/usecases/get-user/get-user';
@@ -8,9 +7,10 @@ import { GetUserUseCase } from '@/usecases/get-user/get-user';
 export class MeUserController implements Controller {
   private authJwt = new AuthenticationJwt();
   private getUser = new GetUserUseCase(new UserMysqlRepository());
+  public accessToken: string = '';
 
-  async handler(request: AuthData): Promise<HttpResponse> {
-    const decoded = await this.authJwt.verify(request.accessToken.split(' ')[1]);
+  async handler(request: any): Promise<HttpResponse> {
+    const decoded = await this.authJwt.verify(this.accessToken.split(' ')[1]);
     const userOrError = await this.getUser.execute({ id: decoded.userId });
     if (userOrError.isLeft()) {
       return HttpHelper.notFound(userOrError.value);
