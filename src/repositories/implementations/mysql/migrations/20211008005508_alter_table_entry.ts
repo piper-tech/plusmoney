3ea3550728e349fd6fccc0ecbe18dfeda4c7af97
Gustavo <alves.gustavocunha@gmail.com>
2021-10-07 22:22:38 -0300

    fix: allow to categoryId to be null and add dropForeign

--- src/repositories/implementations/mysql/migrations/20211008005508_alter_table_entry.ts
+++ src/repositories/implementations/mysql/migrations/20211008005508_alter_table_entry.ts
@@ -2,12 +2,13 @@ import { Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
   return knex.schema.alterTable('entries', (table) => {
-    table.integer('categoryId').unsigned().notNullable().references('id').inTable('categories');
+    table.integer('categoryId').unsigned().references('id').inTable('categories');
   });
 }
 
 export async function down(knex: Knex): Promise<void> {
   return knex.schema.alterTable('entries', (table) => {
+    table.dropForeign('categoryId');
     table.dropColumn('categoryId');
   });
 }
