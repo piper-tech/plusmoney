212e82dfea678773fc1429c612ac89d4c9d2aa61
Gustavo <alves.gustavocunha@gmail.com>
2021-10-07 23:36:35 -0300

    feat: handle usecases response in get-entry-controller

--- src/controllers/implementations/get-entry-controller.ts
+++ src/controllers/implementations/get-entry-controller.ts
@@ -1,17 +1,33 @@
 import { Controller, HttpResponse } from '@/controllers';
-import { EntryMysqlRepository } from '@/repositories/implementations';
+import { CategoryMysqlRepository, EntryMysqlRepository } from '@/repositories/implementations';
+import { GetCategoryUseCase } from '@/usecases/get-category';
 import { GetEntryData, GetEntryUseCase } from '@/usecases/get-entry';
 import { HttpHelper } from '../helpers';
 
 export class GetEntryController implements Controller {
   private getEntry = new GetEntryUseCase(new EntryMysqlRepository());
+  private getCategory = new GetCategoryUseCase(new CategoryMysqlRepository());
 
   async handler(request: GetEntryData): Promise<HttpResponse> {
     const getEntryOrError = await this.getEntry.execute(request);
     if (getEntryOrError.isLeft()) {
       return HttpHelper.badRequest(getEntryOrError.value);
     }
-    const entries = getEntryOrError.value;
+    const entries = await Promise.all(
+      getEntryOrError.value.map(async (entry) => {
+        const categoryOrError = await this.getCategory.execute({ id: entry.categoryId as number, userId: entry.userId });
+        if (categoryOrError.isLeft()) {
+          entry.category = { description: 'Não categorizado' };
+        } else {
+          const category = categoryOrError.value[0];
+          entry.category = { description: category.description };
+        }
+        delete entry.userId;
+        entry.category.id = entry.categoryId;
+        delete entry.categoryId;
+        return entry;
+      })
+    );
     return HttpHelper.ok(entries);
   }
 }
