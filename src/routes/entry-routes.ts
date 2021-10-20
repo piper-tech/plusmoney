d10b5a1339a158e8125819663b9dcca2e945be8d
Gustavo <alves.gustavocunha@gmail.com>
2021-10-19 23:28:26 -0300

    feat: add update entry router

--- src/routes/entry-routes.ts
+++ src/routes/entry-routes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { CreateEntryController, GetEntryController } from '@/controllers/implementations';
+import { CreateEntryController, GetEntryController, UpdateEntryController } from '@/controllers/implementations';
 import { routerAdapter, middlewareAdapter } from '@/routes/adapters';
 import { AuthUserMiddleware } from '@/middlewares/implementations';
 
 export default (router: Router) => {
   router.post('/entries', middlewareAdapter(new AuthUserMiddleware()), routerAdapter(new CreateEntryController()));
   router.get('/entries', middlewareAdapter(new AuthUserMiddleware()), routerAdapter(new GetEntryController()));
+  router.put('/entries/:id', middlewareAdapter(new AuthUserMiddleware()), routerAdapter(new UpdateEntryController()));
 };
