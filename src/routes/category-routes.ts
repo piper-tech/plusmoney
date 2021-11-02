eaae472120a2d02433da4518691748cf0edd1743
Gustavo <alves.gustavocunha@gmail.com>
2021-11-02 17:20:22 -0300

    feat: add update category route

--- src/routes/category-routes.ts
+++ src/routes/category-routes.ts
@@ -2,8 +2,10 @@ import { Router } from 'express';
 import { CreateCategoryController, GetCategoryController } from '@/controllers/implementations';
 import { routerAdapter, middlewareAdapter } from '@/routes/adapters';
 import { AuthUserMiddleware } from '@/middlewares/implementations';
+import { UpdateCategoryController } from '@/controllers/implementations/update-category-controller';
 
 export default (router: Router) => {
   router.post('/categories', middlewareAdapter(new AuthUserMiddleware()), routerAdapter(new CreateCategoryController()));
   router.get('/categories', middlewareAdapter(new AuthUserMiddleware()), routerAdapter(new GetCategoryController()));
+  router.put('/categories/:id', middlewareAdapter(new AuthUserMiddleware()), routerAdapter(new UpdateCategoryController()));
 };
