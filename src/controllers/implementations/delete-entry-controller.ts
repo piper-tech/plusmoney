e14bcd6ba7c9258b75e113966295fe74be1b1027
Gustavo <alves.gustavocunha@gmail.com>
2021-11-11 08:39:05 -0300

    fix: add type to handler function in delete-entry-controller

--- src/controllers/implementations/delete-entry-controller.ts
+++ src/controllers/implementations/delete-entry-controller.ts
@@ -1,12 +1,12 @@
 import { EntryMysqlRepository } from '@/repositories/implementations';
 import { DeleteEntryUseCase, DeleteEntryData } from '@/usecases/delete-entry';
-import { Controller } from '..';
+import { Controller, HttpResponse } from '..';
 import { HttpHelper } from '../helpers';
 
 export class DeleteEntryController implements Controller {
   private deleteEntry = new DeleteEntryUseCase(new EntryMysqlRepository());
 
-  async handler(data: DeleteEntryData) {
+  async handler(data: DeleteEntryData): Promise<HttpResponse> {
     const deletedOrError = await this.deleteEntry.execute(data);
     if (deletedOrError.isLeft()) {
       return HttpHelper.badRequest(deletedOrError.value);
