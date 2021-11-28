2ae8ccf2af46cd3330e16cae8b9976001b9e453e
Gustavo <alves.gustavocunha@gmail.com>
2021-11-28 18:35:05 -0300

    feat: check if category is uncategorized before editing

--- src/usecases/update-category/update-category.ts
+++ src/usecases/update-category/update-category.ts
@@ -16,11 +16,23 @@ export class UpdateCategoryUseCase {
     if (categoryOrError.isLeft()) {
       return left(categoryOrError.value);
     }
+    if (await this.checkIsUncategorized(data)) {
+      return left(new Error('it is not possible to edit this category'));
+    }
     const successOrError = await this.categoryRepository.update(data);
     if (successOrError.isLeft()) {
       return left(new Error('there was an error in the database operation'));
     }
     delete data.id;
     return right(data);
+  }
+
+  private async checkIsUncategorized(data: CategoryData): Promise<boolean> {
+    const categoryOrError = await this.categoryRepository.find({ id: data.id });
+    if (categoryOrError.isLeft()) {
+      return false;
+    }
+    const category = categoryOrError.value[0];
+    return category.description.toLowerCase() === 'não categorizado';
   }
 }
