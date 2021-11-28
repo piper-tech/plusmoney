9c2848f68d82cec0bc28ad872abe283ce694c156
Gustavo <alves.gustavocunha@gmail.com>
2021-11-28 12:42:38 -0300

    fix: register default category

--- src/usecases/create-user/create-user.ts
+++ src/usecases/create-user/create-user.ts
@@ -1,4 +1,4 @@
-import { UserRepository } from '@/repositories';
+import { CategoryRepository, UserRepository } from '@/repositories';
 import { UserData } from '@/entities/data-transfer-objects';
 import { User } from '@/entities';
 import { left, right } from '@/shared';
@@ -7,8 +7,11 @@ import { CreateUserResponse } from './create-user-response';
 
 export class CreateUserUseCase {
   private userRepository: UserRepository;
-  constructor(userRepository: UserRepository) {
+  private categoryRepository: CategoryRepository;
+
+  constructor(userRepository: UserRepository, categoryRepository: CategoryRepository) {
     this.userRepository = userRepository;
+    this.categoryRepository = categoryRepository;
   }
 
   async execute(data: UserData): Promise<CreateUserResponse> {
@@ -24,6 +27,10 @@ export class CreateUserUseCase {
     const createdOrError = await this.userRepository.save(data);
     if (createdOrError.isLeft()) {
       return left(createdOrError.value);
+    }
+    const createdDefaultCategoryOrError = await this.categoryRepository.save({ userId: createdOrError.value.id, description: 'Não categorizado' });
+    if (createdDefaultCategoryOrError.isLeft()) {
+      return left(new Error('there was an error registering the default category'));
     }
     return right(createdOrError.value);
   }
