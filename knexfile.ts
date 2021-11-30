e72929e5431371acf8bf1b2e3157d6dfdc3166d9
Gustavo <alves.gustavocunha@gmail.com>
2021-11-30 14:29:43 -0300

    build: add prod connection to database

--- knexfile.ts
+++ knexfile.ts
@@ -7,15 +7,31 @@ interface KnexConfig {
 const knexConfig: KnexConfig = {
 
   development: {
-    client: "mysql2",
+    client: 'mysql2',
     connection: {
-      host : process.env.DB_HOST,
+      host: process.env.DB_HOST,
       database: process.env.DB_NAME,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD
     },
     migrations: {
-      tableName: "knex_migrations",
+      tableName: 'knex_migrations',
+      // eslint-disable-next-line node/no-path-concat
+      directory: `${__dirname}/src/repositories/implementations/mysql/migrations`
+    }
+  },
+
+  production: {
+    client: 'mysql2',
+    connection: {
+      host: process.env.DB_HOST,
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      // eslint-disable-next-line node/no-path-concat
       directory: `${__dirname}/src/repositories/implementations/mysql/migrations`
     }
   }
